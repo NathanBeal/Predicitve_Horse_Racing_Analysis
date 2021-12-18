3a199705314fd14b68fe2b5657fda5f88b10bfee
NathanBeal14 <nathanielbeal14@gmail.com>
2021-12-17 20:43:17 -0500

    Python program able to successfully read from the csv

--- 3. Python ML Predictions/Machine Learning.py
+++ 3. Python ML Predictions/Machine Learning.py
@@ -1,12 +1,7 @@
 import os
 import pandas as pd
 
-print(os.path.dirname(os.path.abspath(__file__)))
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-curr_dir = curr_dir.split('3. Python ML Predictions')[0];
-curr_dir = curr_dir.replace("\\", "/")
-curr_dir += '2. Java PDF-Data Conditioner/PHRA/Data.csv';
-print(curr_dir)
+data_dir = (os.path.dirname(os.path.abspath(__file__))).split('3. Python ML Predictions')[0].replace("\\", "/")+ '2. Java PDF-Data Conditioner/PHRA/Data.csv';
 
-df = pd.read_csv(curr_dir, skiprows = 1)
+df = pd.read_csv(data_dir)
 print(df)
